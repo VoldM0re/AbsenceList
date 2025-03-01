836a3a5b91ceaceffce782d4c7700a0b1068674d
unknown <dadasik89@gmail.com>
2025-03-01 22:21:23 +0300

    Изменения: Добавлены пресеты, переработаны стили
    Новый функционал:
    - Можно выбрать готовую причину: "Работает", "Болеет" и "По семейным"
    - При нажатии на кнопку "Скопировать" проигрывается анимация звёздочки
    - Текст на этой кнопке возвращается в прежнее состояние через 2 секунды
    Изменения:
    - Оптимизирован код, улучшена его читаемость
    - Список на десктопных экранах стал меньше, убрана излишняя адаптивность
    - Убраны анимации при раскрытии карточки студента

--- main.js
+++ main.js
@@ -33,49 +33,100 @@ let students = [
 // Шаблон карточки студента
 const studentCard = `
 <label class='student-card' style='background-color: hsl(%colorIncrement%, 100%, 46%);'>
-    <div class='name-check'><input class='checkbox' value='%index%' type='checkbox'> %position%. %student%</div>
-    <input class='reason hidden' placeholder='Причина отсутствия?' type='text'>
+    <div class='name-check'>
+        <input class='checkbox' value='%index%' type='checkbox'> %position%. %student%
+    </div>
+    <div class='hidden'>
+        <input class='reason' placeholder='Причина отсутствия?' type='text'>
+        <form class="presets">
+            <label class='preset'>
+                <input name='presets' type='radio'>
+                <span>Работает</span>
+            </label>
+            <label class='preset'>
+                <input name='presets' type='radio'>
+                <span>Болеет</span>
+            </label>
+            <label class='preset'>
+                <input name='presets' type='radio'>
+                <span>По семейным</span>
+            </label>
+        </form>
+    </div>
 </label>`;
 
 // На каждого студента цветовой тон смещается на colorIncrement вправо
 let colorIncrement = 0;
 for (const student of students) {
     const index = students.indexOf(student);
-    document.getElementById('main').innerHTML += studentCard
+    document.getElementById('main').insertAdjacentHTML('beforeend', studentCard
         .replace('%student%', student)
         .replace(/%index%/g, index)
         .replace(/%position%/g, index + 1)
-        .replace('%colorIncrement%', colorIncrement);
-
+        .replace('%colorIncrement%', colorIncrement));
     colorIncrement += 10;
 }
 
-// Сперва заполняется список отсутсвтующих с причинами, затем в его начало добавляется элемент с их кол-вом
-let copyButton = document.getElementById('button');
+const copyButton = document.getElementById('button');
+const checkboxes = document.querySelectorAll('.checkbox');
+let absents = [];
+
+// Сначала заполняется список отсутсвтующих с причинами, затем в его начало добавляется элемент с их кол-вом
 function copyToClipboard() {
-    let absent = [];
-    for (checkbox of document.getElementsByClassName('checkbox')) {
+    absents.length = 0;
+    let presetText = '';
+    for (const checkbox of checkboxes) {
         if (checkbox.checked) {
-            let reason = checkbox.parentNode.parentNode.lastElementChild.value.trim();
-            reason != ''
-                ? absent.push(`${students[checkbox.value]} (${reason})`)
-                : absent.push(students[checkbox.value]);
+            const hidden = checkbox.closest('.student-card').querySelector('.hidden');
+            const presets = hidden.querySelectorAll('.preset');
+
+            for (const preset of presets) {
+                if (preset.querySelector('input[type="radio"]').checked) {
+                    presetText = preset.lastElementChild.textContent;
+                    break;
+                }
+            }
+
+            const fieldText = hidden.querySelector('.reason').value.trim();
+            const studentId = students[checkbox.value]
+            if (fieldText) {
+                absents.push(`${studentId} (${fieldText})`);
+            } else if (presetText) {
+                absents.push(`${studentId} (${presetText})`);
+            } else {
+                absents.push(studentId);
+            }
+
         }
     }
 
-    absent.unshift(`Число отсутствующих студентов: ${absent.length}`);
-    navigator.clipboard.writeText(absent.join('\n'));
-    copyButton.innerText = 'Скопировано!';
+    absents.unshift(`Число отсутствующих студентов: ${absents.length}`);
+    navigator.clipboard.writeText(absents.join('\n')).then(() => {
+        copyButton.innerText = 'Скопировано!';
+        starAnimation();
+        setTimeout(() => {
+            copyButton.innerText = 'Скопировать';
+        }, 2000);
+    });
 }
 
-// Слушает каждый чекбокс, берёт последний элемент родителя (student-card)
-// и делает видимыи последний элемент - текстовое поле
-let checkboxes = document.getElementsByClassName("checkbox");
-for (let i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener("change", function () {
-        var textField = this.parentNode.parentNode.lastElementChild;
-        this.checked
-            ? textField.style.display = "block"
-            : textField.style.display = "none";
+// Слушает каждый чекбокс, берёт student-card и делает видимым элемент .hidden
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', () => {
+        const hidden = checkbox.closest('.student-card').querySelector('.hidden');;
+        hidden.style.display = checkbox.checked ? 'flex' : 'none';
     });
+});
+
+
+function starAnimation() {
+    const star = document.getElementById('star');
+    star.style.display = 'block';
+    star.classList.add('animate');
+
+    setTimeout(() => {
+        star.style.display = 'none';
+        star.classList.remove('animate');
+    }, 2000);
+
 }
